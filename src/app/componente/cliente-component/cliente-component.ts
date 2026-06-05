da203e0d7856a6c977692c5635816ce581367b8b
Eduardo Thomaz <duuhzinn23@gmail.com>
2026-06-05 15:20:02 -0300

    05/06/2026 - MELHORANDO LAYOUT

--- src/app/componente/cliente-component/cliente-component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-cliente-component',
-  imports: [],
-  templateUrl: './cliente-component.html',
-  styleUrl: './cliente-component.css',
-})
-export class ClienteComponent {}
